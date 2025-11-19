ebc79d1101be7a8da337b18602e0884604235a6c
tuilahieu <hieuquahay123@gmail.com>
2025-11-19 19:12:21 +0700

    cc

--- src/app.js
+++ src/app.js
@@ -22,7 +22,14 @@ import subjectClassRoutes from "./routes/subject_class.routes.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
